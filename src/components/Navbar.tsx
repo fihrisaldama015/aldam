0a99a378965be3e4f88576c03f19b1d2046adc6f
Muhamad Fihris Aldama <90702791+fihrisaldama015@users.noreply.github.com>
2025-03-03 13:52:49 +0700

    change domain name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ function Navbar() {
     <nav className="py-4 sm:px-5 px-4 flex justify-between items-center">
       <div className="relative flex gap-4">
         <div className="red-1 absolute right-0 -bottom-[200px] bg-sky-600/20 w-[150px] h-[150px] rounded-full blur-2xl"></div>
-        <h1 className="font-semibold text-[#1e293b]">aldam.dev</h1>
+        <h1 className="font-semibold text-[#1e293b]">fihrisaldama.com</h1>
       </div>
       <div className="blue-container relative">
         <p className="kelas m-0 text-base font-semibold"></p>
